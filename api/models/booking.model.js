95ffbdc21bfa69cc68f6fdec0335671c1832f2c2
Upul <upulkr4@gmail.com>
2023-12-18 10:36:32 +0530

    vercel

--- api/models/booking.model.js
+++ api/models/booking.model.js
@@ -1,35 +1,31 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  time: {
-    type:String,
-    required: true,
-    unique: true,
-  },
   email: {
     type: String,
+    required: true
+  },
+  name: {
+    type: String,
+  
+  },
+  time: {
+    type: String,
     required: true,
-    
-    
-    
+    unique: true,
   },
   date: {
     type: Date,
     required: true,
-    
   },
-  
   phone: {
     type: Number,
-    required: true,
+   
     unique: true,
   },
 });
 
-const Booking=mongoose.model("User",userSchema)
 
-export default Booking
+const Booking = mongoose.model("User", userSchema);
+
+export default Booking;
